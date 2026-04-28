16ba0ca741b40f29d979fc805c22d0d729034b7a
chengwang1009 <chengwang@zumaxdigital.com>
2026-04-28 09:00:20 +0800

    .

--- next.config.mjs
+++ next.config.mjs
@@ -21,6 +21,7 @@ const nextConfig = {
     CURR_USER_ID: "user_id",
     // API_URL: "https://clone.crm-server-eh2y9u82.com/v1",
     // API_URL: "http://localhost:8080/v1",
+    
     API_URL: "https://crm-server-eh2y9u82.com/v1",
     
      FIREBASE_API_KEY: "AIzaSyBE4XGHiR8N5mQlVAFRDYwuIgYrpI3DJLQ",
